44e2edaa80217d95b99490f267e2b0aad5e3600f
rohang13 <rohan13giri@gmail.com>
2019-08-03 23:42:04 +0530

    Weather commit

---
+++ src/app/Search-Stock.ts
@@ -0,0 +1,7 @@
+import { Search } from './search';
+export const SEARCH : Search[] = [
+	{SearchElement:'Bangalore', val:[25,26.3,24.6,27.3,21.5,25.5,24.2],SearchTime:''},
+	{SearchElement:'Chennai',val:[30.2,32.5,31.7,31.1,33.2,35.8,32.5],SearchTime:''},
+	{SearchElement:'Delhi',val:[33.2,35.8,32.5,33.2,30.2,32.5,31.7,31.1],SearchTime:''},
+	{SearchElement:'Pune',val:[29.5,30.1,32.9,33.6,34.8,35.9,34.6],SearchTime:''}
+];
